3b96150dd9db219b09e0cbccdc621ecde75e5b21
Madson Santos <madsonsamsung@gmail.com>
2020-09-01 11:28:15 -0300

    Estrutura do Banner do filme em destaque

--- src/components/featuredMovie.js
+++ src/components/featuredMovie.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './featuredMovie.css';
 
-export default () => {
+export default ({item}) => {
+
+    let firstDate = new Date(item.first_air_date);
+    let genres = [];
+    for (let i in item.genres) {
+        genres.push(item.genres[i].name);
+    }
 
     return (
-        <div>
-            Filme em destaque
-        </div>
+        <section className='featured' style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className='featured_vertical'>
+                <div className='featured_horizontal'>
+                    <div className='featured_name'>{item.original_name}</div>
+                    <div className='featured_info'>
+                        <div className='featured_points'>{item.vote_average}</div>
+                        <div className='featured_year'>{firstDate.getFullYear()}</div>
+                        <div className='featured_seasons'>{item.number_of_seasons} Temporada{item.number_of_seasons  !== 1 ? 's': ''}</div>
+                        <div className='featured_description'>{item.overview}</div>
+
+                        <div className='featured_buttons'>
+                            <a className='featured_watchButton' href={`/watch/${item.id}`}>► Assistir</a>
+                            <a className='featured_mylistButton' href={`/list/add/${item.id}`}>+ Minha Lista</a>
+                        </div>
+
+                        <div className='featured_genres'><strong>Gêneros:</strong> {genres.join(', ')}.</div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
