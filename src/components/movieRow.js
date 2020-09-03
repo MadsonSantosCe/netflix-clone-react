f74acfd0748539caccee6b8fea16d970f7117b25
Madson Santos <madsonsamsung@gmail.com>
2020-09-03 09:27:33 -0300

    Criando animação da lista de filmes

--- src/components/movieRow.js
+++ src/components/movieRow.js
@@ -7,21 +7,22 @@ export default ({title, items}) => {
 
     const [scrollX, setScrollX] = useState(0);
 
-    const handleLeftArrow = () => {
-        let x = scrollX + Math.round(window.innerWidth / 2);
-        if(x > 0){
-            x = 0;
-        }
+    const handleLeftArrow = () => {    
 
-        setScrollX(x);
-    }
-
-    const handleRightArrow = () => {
         let x = scrollX - Math.round(window.innerWidth / 2);
         let listW = items.results.length * 200;
         if((window.innerWidth - listW) > x){
             x = (window.innerWidth - listW) - 60;
         }
+        setScrollX(x);
+    }
+
+    const handleRightArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0){
+            x = 0;
+        }
+
         setScrollX(x);
     }
 
