98fd2a2a5090f4862fd79eb58563a81332ee1a13
Madson Santos <madsonsamsung@gmail.com>
2020-09-03 09:19:06 -0300

    Criando animação da lista de filmes

--- src/components/movieRow.js
+++ src/components/movieRow.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import './movieRow.css';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 export default ({title, items}) => {
+
+    const [scrollX, setScrollX] = useState(0);
+
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0){
+            x = 0;
+        }
+
+        setScrollX(x);
+    }
+
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW = items.results.length * 200;
+        if((window.innerWidth - listW) > x){
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollX(x);
+    }
 
     return (
         <div className='movieRow'>
             <h2>{title}</h2>
+            <div className='movieRow_left' onClick={handleLeftArrow}>               
+               <NavigateNextIcon style={{fontSize: 50}} />
+            </div>
+
+            <div className='movieRow_right' onClick={handleRightArrow}>
+                <NavigateBeforeIcon style={{fontSize: 50}} />   
+            </div>
+
             <div className='movieRow_listArea'>
-                <div className='movieRow_list'>
+                <div className='movieRow_list' style={{
+                    marginLeft: scrollX,
+                    width: items.results.length * 200
+                }}>
                 
                     {items.results.length > 0 && items.results.map((item, key)=>(
                         <div key={key} className='movieRow_item'>
